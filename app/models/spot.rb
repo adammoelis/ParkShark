bb604254085023165058993049b3338a76f5f8ab
Adam Moelis <adam.moelis@gmail.com>
2015-09-29 17:28:06 -0400

    add longitude and latitude to spot model

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,4 +1,6 @@
 class Spot < ActiveRecord::Base
   belongs_to :owner, :class_name => 'User'
   has_many :reviews
+  geocoded_by :address
+  after_validation :geocode
 end
---
+++ db/migrate/20150929212623_add_latitude_and_longitude_to_spot.rb
@@ -0,0 +1,6 @@
+class AddLatitudeAndLongitudeToSpot < ActiveRecord::Migration
+  def change
+    add_column :spots, :latitude, :float
+    add_column :spots, :longitude, :float
+  end
+end
