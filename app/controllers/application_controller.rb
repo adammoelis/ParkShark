c21d49368b48e4e7dd0f9677e8e7ac23b7963b95
Tyler Machen <tylermachen@gmail.com>
2015-09-29 17:50:18 -0400

    remove password requirement from edit profile page and add current_user? to helper methods in application ccontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :resource_name, :resource, :devise_mapping
-  before_filter :configure_permitted_parameters, if: :devise_controller? # may not need before_filter?
-
+  helper_method :resource_name, :resource, :devise_mapping, :current_user?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   def resource_name
     :user
   end
@@ -17,10 +16,14 @@ class ApplicationController < ActionController::Base
     @devise_mapping ||= Devise.mappings[:user]
   end
 
-  protected
-
-  def configure_permitted_parameters # may not need this method
+  private
+  
+  def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :location, :phone, :gender, :birthday, :avatar, :password, :current_password) }
+  end
+
+  def current_user?
+    current_user.id == @user.id
   end
 end
