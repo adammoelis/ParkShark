173aaa51646abd49ee0a60b8d1f1ac33740bed60
Tyler Machen <tylermachen@gmail.com>
2015-09-29 12:10:38 -0400

    add pages for all users, all spots, user profile, and user profile edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :resource_name, :resource, :devise_mapping
 
+  def resource_name
+    :user
+  end
 
-   def resource_name
-     :user
-   end
+  def resource
+    @resource ||= User.new
+  end
 
-   def resource
-     @resource ||= User.new
-   end
-
-   def devise_mapping
-     @devise_mapping ||= Devise.mappings[:user]
-   end
-
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,34 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_user, only: [:show, :update, :edit]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user
+  end
+
+  def update
+    if @user.update(params[:user])
+      # success
+    else
+      # error handling
+    end
+  end
+
+  def edit
+    @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:avatar, :name, :email, :phone, :location)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
 end
