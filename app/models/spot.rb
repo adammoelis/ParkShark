06d3edad3ba6426c9da22b3e9f8878c32a17f980
Adam Moelis <adam.moelis@gmail.com>
2015-09-29 18:36:55 -0400

    add map

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,9 +2,13 @@ class Spot < ActiveRecord::Base
   belongs_to :owner, :class_name => 'User'
   has_many :reviews
   geocoded_by :full_address
-  after_validation :geocode, :if => :address_changed? 
+  after_validation :geocode, :if => :address_changed?
 
   def full_address
     "#{self.address}, #{self.city}, #{self.state}, #{self.zip_code}"
+  end
+
+  def address_without_street
+    "#{self.city}, #{self.state}, #{self.zip_code}"
   end
 end
