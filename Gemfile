c9fdcb3d4172cd0f92e8f30006a55243f219a1c3
Adam Moelis <adam.moelis@gmail.com>
2015-09-29 18:22:30 -0400

    add google map to hsow page and methods for locations

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 gem 'bootstrap-sass', '~> 3.3.5'
+gem 'dotenv-rails', :groups => [:development, :test]
 
 # enable image uploads
 gem "paperclip", "~> 4.3"
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,6 +1,10 @@
 class Spot < ActiveRecord::Base
   belongs_to :owner, :class_name => 'User'
   has_many :reviews
-  geocoded_by :address
-  after_validation :geocode
+  geocoded_by :full_address
+  after_validation :geocode, :if => :address_changed? 
+
+  def full_address
+    "#{self.address}, #{self.city}, #{self.state}, #{self.zip_code}"
+  end
 end
