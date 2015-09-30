c2990ecf0d29846fe752f0d0569cc93b6985dd39
Tyler Machen <tylermachen@gmail.com>
2015-09-29 20:41:37 -0400

    remove unneeded template content and update styling with ParkShark specific content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,22 @@
 class HomeController < ApplicationController
   def index
-
   end
 
+  def about
+  end
 
-    def resource_name
-     :user
-   end
+  def contact
+  end
 
-     def resource
-       @resource ||= User.new
-     end
+  def resource_name
+   :user
+  end
 
-     def devise_mapping
-       @devise_mapping ||= Devise.mappings[:user]
-     end
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions" }
 
   root to: 'home#index'
+  get 'home/about', to: 'home#about', as: 'about'
+  get 'home/contact', to: 'home#contact', as: 'contact'
 
   resources :transactions
   resources :messages
