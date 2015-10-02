102700b94352c931f589f651c24dbb845b7e7f19
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2015-10-02 10:16:46 -0400

    refactor purchasing

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,14 +1,36 @@
 class PurchasesController < ApplicationController
 
   def checkout
+    find_current_relations
+    nonce = params[:payment_method_nonce]
+    result = braintree_transaction(nonce)
+    if result.success?
+      # pay_owner(get_client_token)
+      @listing.available = false
+      @listing.save
+      @reservation = Reservation.new(owner: @spot.owner, visitor: current_user, spot: @spot)
+      @reservation.save
+      flash[:notice] = "Congrats! You just purchased #{@spot.title} for $#{@spot.price}"
+      redirect_to spot_path(@spot)
+    else
+      flash[:notice] = "Sorry, there was a problem. #{result.message}"
+      redirect_to spot_path(@spot)
+    end
+  end
+
+  private
+
+  def find_current_relations
     @owner = User.find(params[:owner_id])
     @visitor = User.find(params[:visitor_id])
     @spot = Spot.find(params[:spot_id])
     @listing = Listing.find(params[:listing_id])
-    nonce = params[:payment_method_nonce]
-    result = Braintree::Transaction.sale(
+  end
+
+  def braintree_transaction(payment_nonce)
+    Braintree::Transaction.sale(
       :amount => @listing.price,
-      :payment_method_nonce => nonce,
+      :payment_method_nonce => payment_nonce,
       :options => {
         :submit_for_settlement => true
       },
@@ -30,18 +52,7 @@ class PurchasesController < ApplicationController
         :country_code_alpha2 => "US"
       }
     )
-    if result.success?
-      # pay_owner(get_client_token)
-      @listing.available = false
-      @listing.save
-      @reservation = Reservation.new(owner: @spot.owner, visitor: current_user, spot: @spot)
-      @reservation.save
-      flash[:notice] = "Congrats! You just purchased #{@spot.title} for $#{@spot.price}"
-      redirect_to spot_path(@spot)
-    else
-      flash[:notice] = "Sorry, there was a problem. #{result.message}"
-      redirect_to spot_path(@spot)
-    end
   end
+
 
 end
