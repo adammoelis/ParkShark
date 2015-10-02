9d3957d1a02337da9c4809ffa81b0c265ccb4f38
Adam Moelis <adam.moelis@gmail.com>
2015-10-02 19:44:55 -0400

    sort added to custom searches via hidden fields

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,13 +8,7 @@ class SearchController < ApplicationController
     else
       @spots = Spot.near(current_location, Spot.default_search_distance)
     end
-    if sort_type && sort_type == "Price"
-      @spots = spots_with_listings(@spots).sort{|a,b| a.lowest_price_listing.price <=> b.lowest_price_listing.price}
-    elsif sort_type && sort_type == "Closest Time"
-      @spots = spots_with_listings(@spots).sort{|a,b| a.closest_time_listing.beginning_time <=> b.closest_time_listing.beginning_time}
-    elsif sort_type && sort_type == "Closest Spot"
-      @spots = spots_with_listings(@spots).sort{|a,b| a.distance_from_location(current_location[0], current_location[1]) <=> b.distance_from_location(current_location[0], current_location[1])}
-    end
+    @spots = sort(@spots, sort_type) if sort_type
     @title = 'Nearby Parking Spots'
     @subtitle = 'The nearest parking spots are below!'
   end
@@ -33,5 +27,14 @@ class SearchController < ApplicationController
     end
   end
 
+  def sort(spots_array, sort_type)
+    if sort_type && sort_type == "Price"
+      spots_with_listings(@spots).sort{|a,b| a.lowest_price_listing.price <=> b.lowest_price_listing.price}
+    elsif sort_type && sort_type == "Closest Time"
+      spots_with_listings(@spots).sort{|a,b| a.closest_time_listing.beginning_time <=> b.closest_time_listing.beginning_time}
+    elsif sort_type && sort_type == "Closest Spot"
+      spots_with_listings(@spots).sort{|a,b| a.distance_from_location(current_location[0], current_location[1]) <=> b.distance_from_location(current_location[0], current_location[1])}
+    end
+  end
 
 end
