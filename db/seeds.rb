798419698403f7bfecab7721a60cdaacb8fec3d3
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2015-10-03 13:17:51 -0400

    seed all except spot images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
+drive_way_urls = [
+  "http://www.stonepatiosva.com/wp-content/uploads/2013/03/Nice-Driveway-VA.jpg",
+  "http://www.eliteconcreterestoration.com/blog/wp-content/uploads/concrete-driveway-resurface.jpg",
+  "http://www.stonepatiosva.com/wp-content/uploads/2013/02/brick-patio-design-and-construction-process-traditional-brick-pavers-style-in-northern-va-with-gate-and-side-garden-plus-chair-and-brick-house.jpeg",
+  "http://content.angieslist.com/2012/2/8/3196672d-d712-4cc0-8dae-156df809aba3.JPG",
+  "http://www.tampaconcretestaining.com/wp-content/gallery/concrete-driveways/driveway-garage.jpg",
+  "http://dings-n-things.com/wp-content/uploads/2013/07/driveway-oil-stain-removal-before.jpg",
+  "http://www.premierpatiosmn.com/wp-content/uploads/2013/01/Driveway.jpg",
+  "http://static.concretenetwork.com/photo-gallery/images/1200x625Exact/concrete-driveways_8/stamped-concrete-driveway-d-e-contreras-construction_61246.jpg",
+  "http://oppconcretewichita.com/files/2013/06/Triple-Driveway-Approach.jpg",
+  "http://content.angieslist.com/2010/2/16/c0c1cae9-a090-4bc8-a19a-ccbfcb9b71b2.jpg"
+]
 
 10.times do
   User.create(
@@ -16,14 +28,15 @@ end
     title: Faker::Name.title,
     address: Faker::Address.street_address,
     available: true,
-    price: Faker::Number.number(3),
-    owner_id: Faker::Number.number(1),
     city: Faker::Address.city,
     state: Faker::Address.state,
     description: Faker::Lorem.paragraph(6),
     zip_code: Faker::Address.zip_code,
-    beginning_time: Faker::Date.between(2.days.ago, Date.today),
-    ending_time: Faker::Date.forward(23)
+    date: Faker::Date.between(2.days.ago, Date.today),
+    owner_id: User.all.sample.id,
+    latitude: Faker::Address.latitude,
+    longitude: Faker::Address.longitude,
+    picture_file_name: drive_way_urls.sample
   )
 end
 
@@ -43,6 +56,25 @@ end
     rating: Faker::Number.number(1),
     body:Faker::Lorem.paragraph,
     spot_id: Spot.all.sample.id,
-    visitor_id: User.all.sample.id
+    visitor_id: User.all.sample.id,
+    owner_id: User.all.sample.id
+  )
+end
+
+10.times do
+  Listing.create(
+    beginning_time:Faker::Date.between(2.days.ago, Date.today),
+    ending_time: Faker::Date.forward(23),
+    available: true,
+    spot_id: Spot.all.sample.id,
+    price: Faker::Commerce.price
+  )
+end
+
+10.times do
+  Location.create(
+    latitude: Faker::Address.latitude,
+    longitude: Faker::Address.longitude,
+    address: Faker::Address.street_address
   )
 end
