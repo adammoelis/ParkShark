40ebd60c8c814f00df75b3fef58574fe65f75e05
Adam Moelis <adam.moelis@gmail.com>
2015-10-06 12:32:09 -0400

    make constants for dropdown

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,8 +4,16 @@ class Listing < ActiveRecord::Base
   has_one :owner, :class_name => 'User', :through => :spot
   has_one :reservation
 
+  MORNING = "Morning (7am-11am)"
+  AFTERNOON = "Afternoon (11am- 2pm)"
+  LATE_AFTERNOON = "Late Afternoon (2pm-5pm)"
+  EARLY_EVENING = "Early Evening (5pm-8pm)"
+  EVENING = "Evening (8pm-11pm)"
+  LATE_NIGHT = "Late Night (11pm-4am)"
+  EARLY_MORNING = "Early Morning (4am-7am)"
+
   def self.time_of_day_options
-    ["Morning (7am-11am)", "Afternoon (11am- 2pm)", "Late Afternoon(2pm-5pm)", ""]
+    [MORNING, AFTERNOON, LATE_AFTERNOON, EARLY_EVENING, EVENING, LATE_NIGHT, EARLY_MORNING]
   end
 
   def status
