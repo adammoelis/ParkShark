e5731acc71b90e8682c5957ba44abb8eac19bce1
Tyler Machen <tylermachen@gmail.com>
2015-10-20 20:32:40 -0400

    fix bug with email sign up not redirecting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,8 +27,8 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    # request.env['omniauth.origin'] || stored_location_for(resource) || root_path
-    request.env['omniauth.origin'] || stored_location_for(resource) || :back
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    # request.env['omniauth.origin'] || stored_location_for(resource) || :back
   end
 
   private
